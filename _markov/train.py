2b81b5f8894d6f98b55ffc555afcd8c50c360440
Eddy Ashton <edashton@microsoft.com>
2024-07-05 14:13:11 +0000

    Wrangling towards manual tokenisation

--- _markov/train.py
+++ _markov/train.py
@@ -1,4 +1,5 @@
 import math
+from collections import defaultdict
 
 
 def pairs(s):
@@ -9,13 +10,51 @@ def double_letters(s):
     return [s[2 * i : 2 * i + 2] for i in range(math.ceil(len(s) / 2))]
 
 
+MAX_NGRAM_SIZE = 6
+
+
+def ngrams(s, n):
+    for i in range(0, len(s) - n + 1):
+        yield s[i : i + n]
+
+
+def find_tokens(s):
+    counts = defaultdict(int)
+    for ngram_size in range(1, MAX_NGRAM_SIZE):
+        for ngram in ngrams(s, ngram_size):
+            counts[ngram] += 1
+
+    sorted_counts = sorted(list(counts.items()), key=lambda p: p[1], reverse=True)
+
+    tokens = [sc[0] for sc in sorted_counts]
+    return tokens
+
+
+def find_best_token(s, i, ranked_tokens):
+    candidates = [s[i : i + n] for n in range(1, MAX_NGRAM_SIZE)]
+    candidate_indices = [ranked_tokens.index(candidate) for candidate in candidates]
+    index_of_min = candidate_indices.index(min(candidate_indices))
+    return candidates[index_of_min]
+
+
 def tokenise(s):
     if not s.startswith("\n"):
         s = "\n" + s
     if not s.endswith("\n"):
         s = s + "\n"
 
-    return list(s)
+    ranked_tokens = find_tokens(s)
+
+    token_stream = []
+
+    i = 0
+    while i < len(s):
+        next_token = find_best_token(s, i, ranked_tokens)
+        i += len(next_token)
+        token_stream.append(next_token)
+
+    print(f"Tokenised: {s} -> {token_stream}")
+    return token_stream
 
 
 def train_model(filename):
