d211cd57de1265cd5acf3561b8bfad08231f6495
Eddy Ashton <edashton@microsoft.com>
2024-07-05 13:20:35 +0000

    Separate tokenisation step

--- _markov/train.py
+++ _markov/train.py
@@ -9,24 +9,32 @@ def double_letters(s):
     return [s[2 * i : 2 * i + 2] for i in range(math.ceil(len(s) / 2))]
 
 
+def tokenise(s):
+    if not s.startswith("\n"):
+        s = "\n" + s
+    if not s.endswith("\n"):
+        s = s + "\n"
+
+    return list(s)
+
+
 def train_model(filename):
     print(f"Beginning training from {filename}")
 
-    data = open(filename).read().splitlines()
+    data = open(filename).read()
 
+    tokens = tokenise(data)
     model = {}
-    for line in data:
-        src = ["\n"] + double_letters(line) + ["\n"]
-        for this_char, next_char in pairs(src):
-            if this_char not in model:
-                model[this_char] = {}
+    for this_token, next_token in pairs(tokens):
+        if this_token not in model:
+            model[this_token] = {}
 
-            probabilities = model[this_char]
+        probabilities = model[this_token]
 
-            if next_char not in probabilities:
-                probabilities[next_char] = 0
+        if next_token not in probabilities:
+            probabilities[next_token] = 0
 
-            probabilities[next_char] += 1
+        probabilities[next_token] += 1
 
     print(f"Training complete")
 
