92e4b87df3db85582e136bda6768f76fe88768ca
Eddy Ashton <edashton@microsoft.com>
2024-07-05 13:04:54 +0000

    Simple training

--- _markov/train.py
+++ _markov/train.py
@@ -1,2 +1,26 @@
+def pairs(s):
+    return zip(s[:-1], s[1:])
+
+
 def train_model(filename):
-    return {"a": 42}
+    print(f"Beginning training from {filename}")
+
+    data = open(filename).read().splitlines()
+
+    model = {}
+    for line in data:
+        src = "\n" + line + "\n"
+        for this_char, next_char in pairs(src):
+            if this_char not in model:
+                model[this_char] = {}
+
+            probabilities = model[this_char]
+
+            if next_char not in probabilities:
+                probabilities[next_char] = 0
+
+            probabilities[next_char] += 1
+
+    print(f"Training complete")
+
+    return model
