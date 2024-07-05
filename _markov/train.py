9e4c8b9873ab611d9438a376918e5dd62cb88511
Eddy Ashton <edashton@microsoft.com>
2024-07-05 14:42:49 +0000

    Delete unused code

--- _markov/train.py
+++ _markov/train.py
@@ -1,41 +1,8 @@
-import math
-from collections import defaultdict
 import nltk
 
 
 def pairs(s):
     return zip(s[:-1], s[1:])
-
-
-def double_letters(s):
-    return [s[2 * i : 2 * i + 2] for i in range(math.ceil(len(s) / 2))]
-
-
-MAX_NGRAM_SIZE = 6
-
-
-def ngrams(s, n):
-    for i in range(0, len(s) - n + 1):
-        yield s[i : i + n]
-
-
-def find_tokens(s):
-    counts = defaultdict(int)
-    for ngram_size in range(1, MAX_NGRAM_SIZE):
-        for ngram in ngrams(s, ngram_size):
-            counts[ngram] += 1
-
-    sorted_counts = sorted(list(counts.items()), key=lambda p: p[1], reverse=True)
-
-    tokens = [sc[0] for sc in sorted_counts]
-    return tokens
-
-
-def find_best_token(s, i, ranked_tokens):
-    candidates = [s[i : i + n] for n in range(1, MAX_NGRAM_SIZE)]
-    candidate_indices = [ranked_tokens.index(candidate) for candidate in candidates]
-    index_of_min = candidate_indices.index(min(candidate_indices))
-    return candidates[index_of_min]
 
 
 def tokenise(s):
