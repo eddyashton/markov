af9500ba8913415a856dd0aff63a5dd97eb5de7b
Eddy Ashton <edashton@microsoft.com>
2024-07-05 13:16:14 +0000

    Try grouping by double-letters

--- _markov/train.py
+++ _markov/train.py
@@ -1,5 +1,12 @@
+import math
+
+
 def pairs(s):
     return zip(s[:-1], s[1:])
+
+
+def double_letters(s):
+    return [s[2 * i : 2 * i + 2] for i in range(math.ceil(len(s) / 2))]
 
 
 def train_model(filename):
@@ -9,7 +16,7 @@ def train_model(filename):
 
     model = {}
     for line in data:
-        src = "\n" + line + "\n"
+        src = ["\n"] + double_letters(line) + ["\n"]
         for this_char, next_char in pairs(src):
             if this_char not in model:
                 model[this_char] = {}
