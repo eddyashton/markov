c9cb6957c12dea25d20dbf0c812f36ed7ca20ec1
Eddy Ashton <edashton@microsoft.com>
2024-07-05 15:12:41 +0000

    Use huggingface's tokenizer

--- _markov/train.py
+++ _markov/train.py
@@ -1,14 +1,17 @@
-import nltk
+from tokenizers import Tokenizer
+from tokenizers.models import BPE
+from tokenizers.trainers import BpeTrainer
+from tokenizers.pre_tokenizers import Whitespace
 
 
 def pairs(s):
     return zip(s[:-1], s[1:])
 
 
-def tokenise(s):
+def tokenise(s, tokenizer):
     tokens = ["\n"]
     for line in s.splitlines():
-        tokens += nltk.tokenize.SyllableTokenizer().tokenize(line)
+        tokens += tokenizer.encode(line).tokens
         tokens.append("\n")
 
     return tokens
@@ -19,7 +22,11 @@ def train_model(filename):
 
     data = open(filename).read()
 
-    tokens = tokenise(data)
+    tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
+    trainer = BpeTrainer(special_tokens=["[UNK]"], show_progress=False)
+    tokenizer.train([filename], trainer)
+
+    tokens = tokenise(data, tokenizer)
     model = {}
     for this_token, next_token in pairs(tokens):
         if this_token not in model:
